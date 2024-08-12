16c75287715d8061f35f6dd154500ee08ca90adb
Danil101214 <Danilkuznyak30@gmail.com>
2024-08-12 21:57:25 +0200

    Updated Frontend

--- src/Components/BlogsComponent/BlogsComponent.jsx
+++ src/Components/BlogsComponent/BlogsComponent.jsx
@@ -11,7 +11,7 @@ const BlogsComponent = ({blog}) => {
     <article className="blogs__column" style={backgroundColor == "#181A2A" ? {border: "1px solid rgb(36, 37, 53)"} : {border: "1px solid rgb(232, 232, 234)"}}>
         <img src={blog.image} alt={blog.title} className='blogs__column-image' onClick={() => navigate(`${blog.id}`)}/>
         <p className="blogs__column-category">{blog.category}</p>
-        <h2 className="blogs__column-title" style={backgroundColor == "#181A2A" ? {color: "#fff"} : {color: "#000"}} onClick={() => navigate(`/blogs/${blog.id}`)}>{blog.title}</h2>
+        <h2 className="blogs__column-title" style={backgroundColor == "#181A2A" ? {color: "#fff"} : {color: "#000"}} onClick={() => navigate(`${blog.id}`)}>{blog.title}</h2>
         <div className="blogs__column-user user">
             <div className="user__box" onClick={() => navigate(`user/${blog.idUser}`)}>
                 <img src={users[Number(blog.idUser) - 1]?.imageprofile} alt="" className="user__box-image" />
